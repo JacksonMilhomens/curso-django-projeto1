1aa6f5aa0715c2d28521e2db572eeb155feabf5f
Jackson Milhomens <jackson.melo@infracommerce.com.br>
2022-04-15 19:40:06 -0300

    continued learn from tests with python

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
 
@@ -11,12 +10,7 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
-
-    # recipes = Recipe.objects.filter(category__id=category_id, is_published=True,).order_by('-id')
-    
-    # if not recipes:
-    #     raise Http404('Not found 😭')
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))  # noqa : E501
     
     return render(request, 'recipes/pages/category.html', context={
             'recipes': recipes,
