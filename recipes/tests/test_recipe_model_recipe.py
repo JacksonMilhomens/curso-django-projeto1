d3ced019eb56ab1d1e014e9fe16ffd2c6e2c9ffe
Jackson Milhomens <jackson.melo@infracommerce.com.br>
2022-04-17 21:38:09 -0300

    continued learnd about test with pytest

---
+++ recipes/tests/test_recipe_model_recipe.py
@@ -0,0 +1,52 @@
+from django.forms import ValidationError
+from recipes.tests.test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
+from parameterized import parameterized
+
+
+class RecipeModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_lenght(self, field, max_lenght):
+        setattr(self.recipe, field, 'A' * (max_lenght + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
+
+            # - Test with unittest -
+
+            # def test_recipe_fields_max_lenght(self):
+            #     fields = [
+            #         ('title', 65),
+            #         ('description', 165),
+            #         ('preparation_time_unit', 65),
+            #         ('servings_unit', 65),
+            #     ]
+
+            #     for field, max_lenght in fields:
+            #         with self.subTest(field=field, max_lenght=max_lenght):
+            #             setattr(self.recipe, field, 'A' * (max_lenght + 1))
+            #             with self.assertRaises(ValidationError):
+            #                 self.recipe.full_clean()
+
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        self.assertFalse(
+            self.recipe.preparation_steps_is_html,
+            msg='Recipe preparation_steps_is_html is not False'
+        )
+
+    def test_recipe_is_published_is_false_by_default(self):
+        # Changing value default of make_recipe for false
+        self.recipe.is_published = False
+
+        self.assertFalse(
+            self.recipe.is_published,
+            msg='is_published is not False'
+        )
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -36,6 +36,19 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn('Recipe Title', response_content)
         self.assertEqual(len(response_context_recipes), 1)
 
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        # Check if exists recipes
+        self.assertIn(
+            '<h1>No recipes found here 😭</h1>',
+            response.content.decode('utf-8'),
+        )
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
@@ -46,6 +59,30 @@ class RecipeViewsTest(RecipeTestBase):
             )
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_loads_recipe(self):
+        needed_title = 'This is a category test'
+
+        # Need a recipe for this test
+        recipe = self.make_recipe(title=needed_title)
+
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': recipe.category.id})  # noqa
+        )
+        response_content = response.content.decode('utf-8')
+
+        self.assertIn(needed_title, response_content)
+
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': recipe.category.id})  # noqa
+        )
+
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
@@ -54,4 +91,30 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id': 1000})
         )
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_the_correct_recipe(self):
+        needed_title = 'This is a detail page - It load one recipe'
+
+        # Need a recipe for this test
+        recipe = self.make_recipe(title=needed_title)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id})
+        )
+
+        response_content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        self.assertIn(needed_title, response_content)
+
+    def test_recipe_detail_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id})
+        )
+
         self.assertEqual(response.status_code, 404)
