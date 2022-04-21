7bea123e40d5ae7a10a59dd37f7c96b0fb16dfc9
Jackson Milhomens <79667681+JacksonMilhomens@users.noreply.github.com>
2022-04-21 20:27:05 -0300

    continued learn about SQL querys and forms django

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
+from django.db.models import Q
 
 
 def home(request):
@@ -34,8 +35,15 @@ def search(request):
     if not search_term:
         raise Http404
 
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ), is_published=True).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', context={
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     }
     )
